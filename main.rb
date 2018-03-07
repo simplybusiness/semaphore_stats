aea5ca40e59967dc7883a209fffe47a773d37cd3
NJBow <nicholas.j.bowman@hotmail.com>
2018-03-07 14:22:14 +0000

    code refactored

--- main.rb
+++ main.rb
@@ -1,16 +1,15 @@
-require 'capybara/dsl'
-require 'capybara-webkit'
-require 'dotenv/load'
-
-Capybara::Webkit.configure do |config|
-  config.allow_unknown_urls
-  config.ignore_ssl_errors
+commandline_input = ARGV
+if commandline_input.length < 3
+  abort("3 arguments are required in the commandline, you've given #{commandline_input.length}:
+  arg1 = Branch name, arg2 = Start build id, arg3 = Stop build id. (start - stop are in reverse order)
+  e.g I want to see average build time of between build 5 to 15 on the branch foo the commandline should read:
+  bundle exec ruby main.rb foo 15 5")
 end
 
-include Capybara::DSL
+$LOAD_PATH << File.expand_path('..', __FILE__)
 
-Capybara.javascript_driver = :webkit
-Capybara.current_driver = Capybara.javascript_driver
+require 'dotenv/load'
+require 'configure_capybara'
 
 Capybara.app_host = 'https://semaphoreci.com'
 visit '/users/sign_in'
@@ -31,7 +30,6 @@ if require_authentication_code
   click_button 'Proceed'
 end
 
-commandline_input = ARGV
 branch_name = ARGV[0]
 start_build_id = ARGV[1].to_i
 stop_build_id = ARGV[2].to_i
@@ -43,31 +41,26 @@ seconds = 0
 number_of_items_taken_into_account = 0
 number_of_items = 0
 
-if commandline_input.length < 3
-  abort("3 arguments are required in the commandline,you've given #{commandline_input.length}:
-  arg1 = Branch name, arg2 = Start build id, arg3 = Stop build id.
-  e.g I want to see average build time of between build 5 to 15 on the branch foo the commandline should read:
-  bundle exec ruby main.rb foo 15 5")
-end
-
-puts "ARGV: #{ARGV.inspect}"
-
 begin
   page += 1
+
   visit "#{branch_page}?page=#{page}"
   sleep 2
+
   build_item_found = false
   all('div.c-branches-list-item').each do |list_item|
     build_item_found = true
     number_of_items += 1
-    puts "build_id: *#{list_item['id']}-"
+
     current_build_id = list_item['id'].split('_').last.to_i
+    puts "...processing: #{current_build_id}"
+
     if stop_build_id <= current_build_id && current_build_id <= start_build_id
       label_text = list_item.find('div.c-branches-list-item_label-holder a').text
       if label_text.strip == 'PASSED'
         list_item.all('a.c-branches-list-item_time_link').each do |time_link|
           text = time_link.text.strip
-          if text.length == 5
+          if text.length == 5 # e.g. text is "05:45", minutes + seconds of build duration
             number_of_items_taken_into_account += 1
             current_minutes, current_seconds = text.split(':').map {|item| item.to_i}
             minutes += current_minutes
@@ -80,8 +73,9 @@ begin
     end
   end
 end while build_item_found
-minutes = (minutes + seconds / 60.to_f) / number_of_items_taken_into_account.to_f
+
+seconds = (minutes * 60  + seconds) / number_of_items_taken_into_account
 
 puts "Number of all builds: #{number_of_items}"
-puts "Number of builds taken into account: #{number_of_items_taken_into_account}"
-puts "Average minutes: #{minutes}"
+puts "Number of PASSED builds: #{number_of_items_taken_into_account}"
+puts "Average time of PASSED builds: #{seconds / 60}mins #{seconds % 60}secs"
